20f7c4213d70ad1544beab7137898ae64dd48101
notek <notek1314@gmail.com>
2024-01-27 19:59:16 +0800

    Lock menu highlight state

--- src/components/MenuComp.vue
+++ src/components/MenuComp.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="menu">
     <Menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline" :inlineIndent="12" @click="onMenuClick">
-      <Menu.Item key="/account">
+      <Menu.Item key="/manage/account">
         <template #icon>
-          <img src="@/assets/imgs/menu-u.png" alt="" class="menu-icon" v-if="!menuKeys['/account']">
+          <img src="@/assets/imgs/menu-u.png" alt="" class="menu-icon" v-if="!menuKeys['/manage/account']">
           <img src="@/assets/imgs/menu-u-active.png" alt="" class="menu-icon" v-else>
         </template>
         <span>Account Management</span>
       </Menu.Item>
-      <Menu.Item key="/digital-amount">
+      <Menu.Item key="/manage/digital-amount">
         <template #icon>
-          <img src="@/assets/imgs/menu-d.png" alt="" class="menu-icon" v-if="!menuKeys['/digital-amount']">
+          <img src="@/assets/imgs/menu-d.png" alt="" class="menu-icon" v-if="!menuKeys['/manage/digital-amount']">
           <img src="@/assets/imgs/menu-d-active.png" alt="" class="menu-icon" v-else>
         </template>
         <span>Digital Amount</span>
       </Menu.Item>
-      <Menu.Item key="/tat-details">
+      <Menu.Item key="/manage/tat-details">
         <template #icon>
-          <img src="@/assets/imgs/menu-t.png" alt="" class="menu-icon" v-if="!menuKeys['/tat-details']">
+          <img src="@/assets/imgs/menu-t.png" alt="" class="menu-icon" v-if="!menuKeys['/manage/tat-details']">
           <img src="@/assets/imgs/menu-t-active.png" alt="" class="menu-icon" v-else>
         </template>
         <span>TAT Details</span>
@@ -27,22 +27,31 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { Menu } from 'ant-design-vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-const selectedKeys = ref<string[]>(['/account'])
+const selectedKeys = ref<string[]>(['/manage/account'])
 
-const menuKeys = ref<{ [k: string]: boolean }>({ '/account': true, '/digital-amount': false, '/tat-details': false })
-const onMenuClick = ({ key }: any) => {
-  menuKeys.value['/account'] = false
-  menuKeys.value['/digital-amount'] = false
-  menuKeys.value['/tat-details'] = false
+const menuKeys = ref<{ [k: string]: boolean }>({ '/manage/account': true, '/manage/digital-amount': false, '/manage/tat-details': false })
+
+const setMenuKeys = (key: string) => {
+  menuKeys.value['/manage/account'] = false
+  menuKeys.value['/manage/digital-amount'] = false
+  menuKeys.value['/manage/tat-details'] = false
   menuKeys.value[key] = true
-
-  router.push(`/manage${key}`)
 }
+
+const onMenuClick = ({ key }: any) => {
+  setMenuKeys(key)
+  router.push(key)
+}
+
+onMounted(() => {
+  setMenuKeys(router.currentRoute.value.path)
+  selectedKeys.value = [router.currentRoute.value.path]
+})
 </script>
 
 <style lang="less" scoped>
