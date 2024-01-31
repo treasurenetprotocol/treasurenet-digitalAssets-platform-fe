6636025176483cdc8644e9905e0640dd24d7c47f
notek <notek1314@gmail.com>
2024-01-31 17:09:25 +0800

    update: evn settings

--- README.md
+++ README.md
@@ -2,20 +2,32 @@
 
 ## Project Setup
 
+### The nodejs version needs to be greater than or equal to v16.18.0
+
 ```sh
 npm install
 ```
 
-### Compile and Hot-Reload for Development
+### Compile and Hot-Reload for Development (default: localhost env)
 
 ```sh
 npm run dev
+
+npm run dev:test
+
+npm run dev:main
 ```
 
 ### Type-Check, Compile and Minify for Production
 
 ```sh
 npm run build
+
+npm run build:dev
+
+npm run build:test
+
+npm run build:main
 ```
 
 ### Lint with [ESLint](https://eslint.org/)
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), nodePolyfills()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -23,7 +24,7 @@ export default defineConfig({
       "/api": {
         secure: false,
         changeOrigin: true,
-        target: "https://124.70.23.119:3045"
+        target: "https://124.70.23.119:3045",
       },
     },
   },
